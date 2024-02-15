e08417cf52101525ca371b433ded297f870e6313
Aline Antunes <61796612+alinealien@users.noreply.github.com>
2024-02-15 13:35:45 -0300

    Adicionando os arquivos py

---
+++ resolucoes_code/concat_dados.py
@@ -0,0 +1,1 @@
+# Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
---
+++ resolucoes_code/repet_txt.py
@@ -0,0 +1,1 @@
+# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
